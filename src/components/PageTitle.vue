1be21a48a0d49df70fe5f75575c1e4e1a9481e7b
Farzam Noori <farzamn80@gmail.com>
2018-10-24 14:57:11 -0600

    Added pages
    Added title component
    Pages added:
    Computer Graphics
    Databases
    Full Stack

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="TitlePage">
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TitlePage',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/views/Databases.vue
+++ src/views/Databases.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home">
-    <PageTitle title="Home">
+  <div class="databases">
+    <PageTitle title="Databases">
     </PageTitle>
   </div>
 </template>
@@ -9,7 +9,7 @@
 import PageTitle from '@/components/PageTitle.vue'
 
 export default {
-  name: 'home',
+  name: 'databases',
   components: {
     PageTitle
   }
@@ -17,7 +17,7 @@ export default {
 </script>
 
 <style scoped>
-  .home {
+  .databases {
     margin: 0.5rem 1rem;
   }
 </style>
