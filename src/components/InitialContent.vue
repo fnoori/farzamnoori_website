68412b2d155634eb4537de7099e46ef4ea40e4fd
Farzam Noori <farzamn80@gmail.com>
2018-10-24 09:55:59 -0600

    Removed tmp stuff
    Added email and name

---
+++ src/components/InitialContent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="initialContent">
+    <a href="mailto:fnoori431@gmail.com">fnoori431@gmail.com</a>
+    <p>
+      Farzam Noori
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InitialContent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <InitialContent/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import InitialContent from '@/components/InitialContent.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    InitialContent
   }
 }
 </script>
