86e46a4ea767e8507d36ccbf67e366968b07e80b
Farzam Noori <fnoori431@gmail.com>
2019-02-09 18:00:32 -0700

    Removed all traces of store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store/'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -11,7 +10,6 @@ Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 
 new Vue({
-  store,
   router,
   render: h => h(App)
 }).$mount('#app')
